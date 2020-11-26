ad20b4b34192efbcae256bc5bdd961e9017ba304
Devang Kamble <risingentropy20@gmail.com>
2020-11-26 11:56:39 +0530

    Increase Dimensions

--- dankFlappyBird/main.py
+++ dankFlappyBird/main.py
@@ -6,7 +6,7 @@ import sys
 
 pygame.init()
 
-screen = pygame.display.set_mode((800, 500))
+screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 
 pygame.display.set_caption("Dank Flappy Bird")
@@ -14,15 +14,13 @@ icon = pygame.image.load("icon.png")
 pygame.display.set_icon(icon)
 
 background = pygame.image.load("background.png").convert()
+#background = pygame.transform.scale2x(background)
 base = pygame.image.load("base.png").convert()
-base_pos = -500
+base_pos = -250
 
 bird_mid = pygame.image.load("redbird-midflap.png").convert()
 bird_mid = pygame.transform.scale2x(bird_mid)
 bird_rect = bird_mid.get_rect(center = (200, 250))
-
-
-
 
 
 
@@ -33,11 +31,11 @@ while True:
             sys.exit()
 
     screen.fill((57,62,70))
-    screen.blit(background, (0, -70))
+    screen.blit(background, (0, 0))
     base_pos += 1
     if base_pos >= 0:
-        base_pos = -500
-    screen.blit(base, (base_pos, 430))
+        base_pos = -250
+    screen.blit(base, (base_pos, 590))
     screen.blit(bird_mid, bird_rect)
 
 
